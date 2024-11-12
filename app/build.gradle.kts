24a88967355e811b911ff3c72c329704e112a89d
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-12 15:15:58 +0700

    Changing minSdkVersion from 24 to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vokrob.traffic_light_2"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
